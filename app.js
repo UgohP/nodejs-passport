7db02c615311308a3163dabc6139875d0af3e885
Paschal Ugoh <paskyugoh@gmail.com>
2024-08-01 06:58:41 +0100

    done with the app but there are still some corrections should be done on the messages

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
+
 
 const app = express();
+//passport config
+require('./config/passport')(passport);
+
 //DB Config
 const db = require('./config/keys').MongoURI;
 
@@ -15,6 +22,33 @@ mongoose.connect(db, {useNewUrlParser: true})
 //EJS
 app.use(expressLayouts)
 app.set('view engine', 'ejs');
+
+//Bodyparser middleware
+app.use(express.urlencoded({ extended: false }));
+
+//express session
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true,
+ }));
+
+//Passport session
+app.use(passport.initialize());
+app.use(passport.session());
+
+//connect-flash middleware
+ app.use(flash());
+
+ //Global variables
+ app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+ });
+
+//static folder
 
 //routes
 app.use('/', require('./routes/index'))
